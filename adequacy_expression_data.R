5ff560d6bbb118583fca052b678de29b6735578d
fieldima <60451947+fieldima@users.noreply.github.com>
2021-11-24 00:11:27 -0800

    Finished script to calculate arbutus for calibrated trees

--- adequacy_expression_data.R
+++ adequacy_expression_data.R
@@ -11,29 +11,40 @@ library(tidyverse)
 calibrated <- readRDS("export/calibrated_trees")
 
 #Make a function to iterate through each list
-adequacy <- function (tree) {
+adequacy <- function (tree, part) {
+  result <- data.frame(
+    m.sig = vector("double", length = length(tree)),
+    c.var = vector("double", length = length(tree)),
+    s.var = vector("double", length = length(tree)),
+    s.asr = vector("double", length = length(tree)),
+    s.hgt = vector("double", length = length(tree)),
+    d.cdf = vector("double", length = length(tree))
+    )
+  count <- 1
   for (tr in tree){
-    tib <- as_tibble(tr)
-    phylo <- tib %>% select(parent, node, branch.length, label.x) %>%
-      mutate(label = label.x) %>% select(-label.x)
-    dat <- tib %>% select(label.x, Averaged.RPKM.brain:Averaged.RPKM.liver)
+    phylo <- tr@phylo
+    dat <- tr@data %>% select(label, Averaged.RPKM.brain:Averaged.RPKM.liver) %>%
+      slice(1:((nrow(tr@data)+1)/2)) %>% column_to_rownames(var = "label") %>%
+      as.matrix()
+    dat[is.na(dat)] = 0
+    arb <- arbutus(phylo, data = dat[,part])$p.values
+    result$m.sig <- arb[1]
+    result$c.var <- arb[2]
+    result$s.var <- arb[3]
+    result$s.asr <- arb[4]
+    result$s.hgt <- arb[5]
+    result$d.cdf <- arb[6]
+    count <- count + 1
   }
+  result
 }
 
-#Pull entry from list
-cal_entry <- calibrated[[1]]
+#Measure adequacy
+brain <- adequacy(calibrated, 1)
+heart <- adequacy(calibrated, 2)
+kidney <- adequacy(calibrated, 3)
+testis <- adequacy(calibrated, 4)
+cerebellum <- adequacy(calibrated, 5)
+liver <- adequacy(calibrated, 6)
 
-#Convert to tibbles
-cal_tib <- as_tibble(cal_entry)
-
-#Extract phylo objects
-cal_phylo <- cal_tib %>% select(parent, node, branch.length, label.x) %>%
-  mutate(label = label.x) %>% select(-label.x) %>% as.phylo()
-
-#Extract data
-cal_data <- cal_tib %>% select(label.x, Averaged.RPKM.brain:Averaged.RPKM.liver)
-cal_dat <- cal_data %>% slice(1:((nrow(cal_data)+1)/2)) %>% column_to_rownames(var = "label.x") %>%
-  as.matrix()
-
-#Run arbutus
-cal_results <- arbutus(cal_phylo, data = cal_dat[,"Averaged.RPKM.heart"])
+save.image(file = "arbutus/")
